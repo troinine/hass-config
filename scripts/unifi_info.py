5b3259e07f47a1d718706205a0db6d26715cf95b
Tero Roininen <troinine@gmail.com>
2023-02-25 16:05:05 +0300

    fix: unifi_info script fails on version 2.x firmware

--- scripts/unifi_info.py
+++ scripts/unifi_info.py
@@ -36,7 +36,7 @@ print(json.dumps({
     'system_temp': round(data['temperatures'][0]['value'], 1),
     'memory': data['system-stats']['mem'],
     'disk': round(data['storage'][1]['used'] / data['storage'][1]['size'] * 100, 1),
-    'internet': data['internet'],
+    'internet': data['wan1']['up'],
     'uptime': datetime.fromtimestamp(data['startup_timestamp']).isoformat(),
     'availability': data['uptime_stats']['WAN']['availability'],
     'average_latency': data['uptime_stats']['WAN']['latency_average'],
